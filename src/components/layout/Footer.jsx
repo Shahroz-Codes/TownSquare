c3eb4457eec527039f3152d3d7c8de9b997fd47b
Shahroz-Codes <shafqatshahroz@gmail.com>
2025-07-05 17:09:21 +0500

    feat: Update logo import to maintain consistent casing in Footer and Navbar components

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo from "../../assets/logo.png"
+import Logo from "../../assets/Logo.png"
 
 function Footer() {
   return (
 
     <footer className="text-center py-6 text-gray-200 bg-gray-900 text-sm flex justify-center items-center gap-3 sticky bottom-0 z-50">
       &copy; {new Date().getFullYear()}
-      <img src={logo} alt="Logo" className='h-5 w-5' />
+      <img src={Logo} alt="Logo" className='h-5 w-5' />
       TownSquare. All rights reserved.
     </footer>
 
